7064cff5758def93350fd33194e3730615118f53
Uddin02 <mr.uddin360@gmail.com>
2022-11-08 22:33:12 +0600

    footer added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
             <div className="lg:flex flex-row-reverse justify-center items-center">
                 <img className="lg:w-4/12" src={image} alt="" />
                 <div className="lg:w-6/12 mx-2">
-                    <p className="font-bold md:text-2xl lg:text-7xl">Life is short<br/> and the world<br/> is wide!!</p>
-                    <p className="mt-4 lg:w-7/12 lg:text-2xl text-justify">
+                    <p className="font-bold md:text-2xl lg:text-7xl text-gray-800">Life is short<br/> and the world<br/> is wide!!</p>
+                    <p className="mt-4 lg:w-7/12 lg:text-2xl text-gray-800 text-justify">
                         To get of your best adventure you just need to leave and go where you like.
                     </p>
                 </div>
